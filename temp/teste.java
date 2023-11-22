e549efdd0d451db38d8bb0c25f29eb81283482db
DanielBichof <daniel.h.bichof@gmail.com>
2023-11-22 20:31:02 -0300

    init

---
+++ temp/teste.java
@@ -0,0 +1,10 @@
+public public class Principal {
+  public static void main() {
+    String resultado = testarExtracao();
+    System.out.println("resultado: " + resultado);
+  }
+
+  private static String testarExtracao() {
+    return "Oi, mundo!";
+  }
+}
